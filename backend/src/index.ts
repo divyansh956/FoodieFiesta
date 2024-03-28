1953bd9fccbc206b3156703398c01a0789003796
Divyansh Saxena <divyanshsxn998@gmail.com>
2024-03-28 14:58:59 +0530

    database setup

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -2,10 +2,20 @@ import express from "express";
 import cors from "cors";
 import "dotenv/config";
 import { Request, Response } from "express";
+import mongoose from "mongoose";
 
 const app = express();
 app.use(cors());
 app.use(express.json());
+
+mongoose
+  .connect(process.env.MONGO_URL as string)
+  .then(() => {
+    console.log("MongoDB connected");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
 
 app.get("/test", async (req: Request, res: Response) => {
   res.send("Hello");
