4cfe41fa795f89b2faf1f217b87bc6d25587b121
Divyansh Saxena <divyanshsxn998@gmail.com>
2024-03-30 11:13:09 +0530

    user-model

---
+++ backend/src/models/User.ts
@@ -0,0 +1,28 @@
+import mongoose from "mongoose";
+
+const UserSchema = new mongoose.Schema({
+  auth0Id: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  email: {
+    type: String,
+    required: true,
+  },
+  name: {
+    type: String,
+  },
+  adressLine1: {
+    type: String,
+  },
+  city: {
+    type: String,
+  },
+  country: {
+    type: String,
+  },
+});
+
+const User = mongoose.model("User", UserSchema);
+export default User;
