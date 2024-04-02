8b22498fc444549ada0c727475c5e8a2f8e8bff8
Divyansh Saxena <divyanshsxn998@gmail.com>
2024-04-02 12:03:04 +0530

    sort filter option

--- frontend/src/pages/SearchPage.tsx
+++ frontend/src/pages/SearchPage.tsx
@@ -4,6 +4,7 @@ import PaginationSelector from "@/components/PaginationSelector";
 import SearchBar, { SearchForm } from "@/components/SearchBar";
 import SearchResultCard from "@/components/SearchResultCard";
 import SearchResultInfo from "@/components/SearchResultInfo";
+import SortOptionDropdown from "@/components/SortOptionDropdown";
 import { useState } from "react";
 import { useParams } from "react-router-dom";
 
@@ -11,14 +12,16 @@ export type SearchState = {
   searchQuery: string;
   page: number;
   selectedCuisines: string[];
+  sortOption: string;
 };
 
 const SearchPage = () => {
   const { city } = useParams();
-  const [searchState, setsearchState] = useState<SearchState>({
+  const [searchState, setSearchState] = useState<SearchState>({
     searchQuery: "",
     page: 1,
     selectedCuisines: [],
+    sortOption: "bestMatch",
   });
   const [isExpanded, setIsExpanded] = useState<boolean>(false);
   const { results, isLoading } = useSearchRestaurants(searchState, city);
@@ -31,8 +34,16 @@ const SearchPage = () => {
     return <span>No results found</span>;
   }
 
+  const setSortOption = (sortOption: string) => {
+    setSearchState((prevState) => ({
+      ...prevState,
+      sortOption,
+      page: 1,
+    }));
+  };
+
   const setSelectedCuisines = (selectedCuisines: string[]) => {
-    setsearchState((prevState) => ({
+    setSearchState((prevState) => ({
       ...prevState,
       selectedCuisines,
       page: 1,
@@ -40,22 +51,22 @@ const SearchPage = () => {
   };
 
   const setPage = (page: number) => {
-    setsearchState((prevState) => ({
+    setSearchState((prevState) => ({
       ...prevState,
       page,
     }));
   };
 
   const setSearchQuery = (searchFormData: SearchForm) => {
-    setsearchState((prevState) => ({
+    setSearchState((prevState) => ({
       ...prevState,
       searchQuery: searchFormData.searchQuery,
       page: 1,
     }));
   };
 
-  const resetearchQuery = () => {
-    setsearchState((prevState) => ({
+  const resetSearchQuery = () => {
+    setSearchState((prevState) => ({
       ...prevState,
       searchQuery: "",
       page: 1,
@@ -77,9 +88,15 @@ const SearchPage = () => {
           searchQuery={searchState.searchQuery}
           onSubmit={setSearchQuery}
           placeholder="Search by Cuisine or Restaurant Name"
-          onReset={resetearchQuery}
+          onReset={resetSearchQuery}
         />
-        <SearchResultInfo total={results.pagination.total} city={city} />
+        <div className="flex justify-between flex-col gap-3 lg:flex-row">
+          <SearchResultInfo total={results.pagination.total} city={city} />
+          <SortOptionDropdown
+            sortOption={searchState.sortOption}
+            onChange={setSortOption}
+          />
+        </div>
         {results.data.map((restaurant) => (
           <div key={restaurant._id}>
             <SearchResultCard restaurant={restaurant} />
