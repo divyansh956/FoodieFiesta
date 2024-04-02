f14528842ef42008e15aeefdfa7f42b4daf83975
Divyansh Saxena <divyanshsxn998@gmail.com>
2024-04-02 10:15:27 +0530

    search filter

--- frontend/src/pages/SearchPage.tsx
+++ frontend/src/pages/SearchPage.tsx
@@ -1,11 +1,20 @@
 import { useSearchRestaurants } from "@/api/RestaurantApi";
+import SearchBar, { SearchForm } from "@/components/SearchBar";
 import SearchResultCard from "@/components/SearchResultCard";
 import SearchResultInfo from "@/components/SearchResultInfo";
+import { useState } from "react";
 import { useParams } from "react-router-dom";
+
+export type SearchState = {
+  searchQuery: string;
+};
 
 const SearchPage = () => {
   const { city } = useParams();
-  const { results, isLoading } = useSearchRestaurants(city);
+  const [searchState, setsearchState] = useState<SearchState>({
+    searchQuery: "",
+  });
+  const { results, isLoading } = useSearchRestaurants(searchState, city);
 
   if (isLoading) {
     return <span>Loading...</span>;
@@ -15,10 +24,30 @@ const SearchPage = () => {
     return <span>No results found</span>;
   }
 
+  const setSearchQuery = (searchFormData: SearchForm) => {
+    setsearchState((prevState) => ({
+      ...prevState,
+      searchQuery: searchFormData.searchQuery,
+    }));
+  };
+
+  const resetearchQuery = () => {
+    setsearchState((prevState) => ({
+      ...prevState,
+      searchQuery: "",
+    }));
+  };
+
   return (
     <div className="grid grid-cols-1 lg:grid-cols-[250px_1fr] gap-5">
       <div id="cuisines-list">Cuisines</div>
       <div id="main-content" className="flex flex-col gap-5">
+        <SearchBar
+          searchQuery={searchState.searchQuery}
+          onSubmit={setSearchQuery}
+          placeholder="Search by Cuisine or Restaurant Name"
+          onReset={resetearchQuery}
+        />
         <SearchResultInfo total={results.pagination.total} city={city} />
         {results.data.map((restaurant) => (
           <div key={restaurant._id}>
